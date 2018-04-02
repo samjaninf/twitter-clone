80276ac9a9eda699966bfe3eac5b4c2788c4fca2
Robb <kimchibot64@gmail.com>
2018-04-02 16:34:25 -0400

    delete action for auth_controller

--- lib/twitter_web/controllers/auth_controller.ex
+++ lib/twitter_web/controllers/auth_controller.ex
@@ -25,6 +25,12 @@ defmodule TwitterWeb.AuthController do
     end
   end
 
+  def delete(conn, _params) do
+    conn
+    |> configure_session(drop: true)
+    |> redirect(to: page_path(conn, :index))
+  end
+
   defp insert_or_update_user(changeset) do
     case Repo.get_by(User, email: changeset.changes.email) do
       nil ->
