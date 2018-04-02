2e4f3935ba914c436497cb68492e5e7bfded8795
Robb <kimchibot64@gmail.com>
2018-04-02 12:11:14 -0400

    nav test for logout

--- test/twitter_web/templates/layout/navigation_test.exs
+++ test/twitter_web/templates/layout/navigation_test.exs
@@ -1,9 +1,20 @@
 defmodule Twitter.NavigationTest do
-  use TwitterWeb.ConnCase
+  use TwitterWeb.ConnCase, async: true
+  import Twitter.Factory
 
   test "shows a sign in with Google link when not signed in", %{conn: conn} do
     conn = get conn, "/"
 
     assert html_response(conn, 200) =~ "Sign in with Google"
   end
+
+  test "shows a sign out link when signed in", %{conn: conn} do
+    user = insert(:user)
+
+    conn = conn
+    |> assign(:user, user)
+    |> get("/")
+
+    assert html_response(conn, 200) =~ "Sign out"
+end
 end
