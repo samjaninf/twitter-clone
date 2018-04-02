164196621bc1e5bb95d26f7c24d92b1e68201252
Robb <kimchibot64@gmail.com>
2018-04-02 11:16:12 -0400

    added new action to AuthController

--- lib/twitter_web/controllers/auth_controller.ex
+++ lib/twitter_web/controllers/auth_controller.ex
@@ -2,4 +2,10 @@ defmodule TwitterWeb.AuthController do
   use TwitterWeb, :controller
   plug Ueberauth
 
+  def new(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    user_params = %{token: auth.credentials.token, first_name: auth.info.first_name, last_name: auth.info.last_name, email: auth.info.email, provider: "google"}
+    changeset = User.changeset(%User{}, user_params)
+
+    create(conn, changeset)
+  end
 end
