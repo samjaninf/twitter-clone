881210f04405a11b1a88eaf92c39a871da8c37e5
Robb <kimchibot64@gmail.com>
2018-04-02 11:55:38 -0400

    added user_factory

---
+++ test/support/user_factory.ex
@@ -0,0 +1,14 @@
+defmodule Twitter.UserFactory do
+  use ExMachina.Ecto, repo: Twitter.Repo
+  alias Twitter.User
+
+  def user_factory do
+    %User{
+      token: "ffnebyt73bich9",
+      email: "batman@example.com",
+      first_name: "Bruce",
+      last_name: "Wayne",
+      provider: "google"
+    }
+  end
+end
