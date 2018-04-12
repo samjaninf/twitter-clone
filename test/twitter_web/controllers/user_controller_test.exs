450337e9eee13c1520fe9a69cf616f572f3ad2e7
Robb <kimchibot64@gmail.com>
2018-04-12 15:46:20 -0400

    refactor of users tests

--- test/twitter_web/controllers/user_controller_test.exs
+++ test/twitter_web/controllers/user_controller_test.exs
@@ -6,7 +6,9 @@ defmodule TwitterWeb.UserControllerTest do
 
   setup do
     user = insert(:user)
-    {:ok, user: user}
+    valid_user_params = params_for(:user)
+    invalid_user_params = params_for(:user, email: "")
+    {:ok, user: user, valid_user_params: valid_user_params, invalid_user_params: invalid_user_params}
   end
 
   describe "index" do
@@ -24,8 +26,8 @@ defmodule TwitterWeb.UserControllerTest do
   end
 
   describe "create user" do
-    test "redirects to show when data is valid", %{conn: conn} do
-      valid_user_params = params_for(:user)
+    test "redirects to show when data is valid", %{conn: conn, valid_user_params: valid_user_params} do
+
       conn = post conn, user_path(conn, :create), user: valid_user_params
 
       assert %{id: id} = redirected_params(conn)
@@ -35,25 +37,24 @@ defmodule TwitterWeb.UserControllerTest do
       assert html_response(conn, 200) =~ "Show User"
     end
 
-    test "renders errors when data is invalid", %{conn: conn} do
-      invalid_user_params = params_for(:user, email: "")
+    test "renders errors when data is invalid", %{conn: conn, invalid_user_params: invalid_user_params} do
+
       conn = post conn, user_path(conn, :create), user: invalid_user_params
       assert html_response(conn, 200) =~ "New User"
     end
   end
 
   describe "edit user" do
-    test "renders form for editing chosen user", %{conn: conn} do
-      user = insert(:user)
+    test "renders form for editing chosen user", %{conn: conn, user: user} do
+
       conn = get conn, user_path(conn, :edit, user)
       assert html_response(conn, 200) =~ "Edit User"
     end
   end
 
   describe "update user" do
-    test "redirects when data is valid", %{conn: conn} do
-      user = insert(:user)
-      valid_user_params = params_for(:user)
+    test "redirects when data is valid", %{conn: conn, user: user, valid_user_params: valid_user_params} do
+
       conn = put conn, user_path(conn, :update, user), user: valid_user_params
       assert redirected_to(conn) == user_path(conn, :show, user)
 
@@ -61,17 +62,16 @@ defmodule TwitterWeb.UserControllerTest do
       assert html_response(conn, 200) =~ valid_user_params.email
     end
 
-    test "renders errors when data is invalid", %{conn: conn, user: user} do
-      user = insert(:user)
-      invalid_user_params = params_for(:user, email: "")
+    test "renders errors when data is invalid", %{conn: conn, user: user, invalid_user_params: invalid_user_params} do
+
       conn = put conn, user_path(conn, :update, user), user: invalid_user_params
       assert html_response(conn, 200) =~ "Edit User"
     end
   end
 
   describe "delete user" do
-    test "deletes chosen user", %{conn: conn} do
-      user = insert(:user)
+    test "deletes chosen user", %{conn: conn, user: user} do
+
       conn = delete conn, user_path(conn, :delete, user)
       assert redirected_to(conn) == user_path(conn, :index)
       assert_error_sent 404, fn ->
